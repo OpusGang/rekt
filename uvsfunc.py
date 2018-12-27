8b03e0ad1a823ad15ff4f88d0d55155ed98dc06e
Ututu <daoko@protonmail.com>
2018-12-27 15:05:24 +0000

    uvsfunc.py typo fixes

--- uvsfunc.py
+++ uvsfunc.py
@@ -2,12 +2,15 @@ from vapoursynth import core
 import vapoursynth as vs
   
 '''    
-Creates a rectangular "mask" for a fix to be applied to.  Cropped area must be mod 4.
+Creates a rectangular "mask" for a fix to be applied to.  Cropped area must be mod 4 for YUV420 or mod 2 for YUV422.
 '''
 def rekt(src, fix, left=0, right=0, top=0, bottom=0):
-	if src.format == vs.YUV420P8 or vs.YUV420P10 or vs.YUV420P12 or vs.YUV420P16 or vs.YUV420P32 or vs.YUV422P8 or vs.YUV422P10 or vs.YUV422P12 or vs.YUV422P16 or vs.YUV422P32:
+	if src.format == vs.YUV420P8 or vs.YUV420P10 or vs.YUV420P12 or vs.YUV420P16 or vs.YUV420P32:
 	    if src.width-left-right % 4 != 0 or src.height-top-bottom % 4 != 0:
-			raise TypeError("rekt_fast: fix height and width must be mod 4.")
+			raise TypeError("rekt: fix height and width must be mod 4 if source clip's color space is YUV420.")
+	if src.format == vs.YUV422P8 or vs.YUV422P10 or vs.YUV422P12 or vs.YUV422P16 or vs.YUV422P32:
+	    if src.width-left-right % 4 != 0 or src.height-top-bottom % 2 != 0:
+			raise TypeError("rekt: fix height and width must be mod 2 if source clip's color space is YUV422.")
 	else:
 		if left > 0 or right > 0:
 		    m = core.std.Crop(fix, left=left, right=right)
@@ -25,7 +28,8 @@ def rekt(src, fix, left=0, right=0, top=0, bottom=0):
   
 '''
 Creates a rectangular "mask" in which a fix is applied to only the masked area.  This vastly speeds up filters like anti-aliasing and scaling.
-This works with a lambda function called m, which is applied to the "m" clip later on:
+Cropped area must be mod 4 for YUV420 or mod 2 for YUV422
+This wrapper works with a lambda function called m, which is applied to the "m" clip later on:
 
 rekt_fast(src, fun = lambda m: taa.TAAmbk(m, aatype=3, preaa=-1, strength=-1, masktype=2), left=2, right=8, top=10, bottom=2)
 
@@ -33,10 +37,14 @@ Results in the following function being applied to only the masked rectangle:
 
 def f(m):
     return taa.TAAmbk(m, aatype=3, preaa=-1, strength=-1, masktype=2)
-    
-There is currently no mod 4 protection here.
 '''
 def rekt_fast(src, fun=lambda x: x, left=0, right=0, top=0, bottom=0):
+	if src.format == vs.YUV420P8 or vs.YUV420P10 or vs.YUV420P12 or vs.YUV420P16 or vs.YUV420P32:
+	    if src.width-left-right % 4 != 0 or src.height-top-bottom % 4 != 0:
+			raise TypeError("rekt_fast: fix height and width must be mod 4 if source clip's color space is YUV420.")
+	if src.format == vs.YUV422P8 or vs.YUV422P10 or vs.YUV422P12 or vs.YUV422P16 or vs.YUV422P32:
+	    if src.width-left-right % 4 != 0 or src.height-top-bottom % 2 != 0:
+			raise TypeError("rekt_fast: fix height and width must be mod 2 if source clip's color space is YUV422.")
     m = core.std.Crop(src, left=left, right=right, bottom=bottom, top=top)
     if left > 0 or right > 0:
         m = fun(m).std.AddBorders(top=top, bottom=bottom)
@@ -66,10 +74,12 @@ def rektaa(clip, left=0, right=0, top=0, bottom=0, aatype=3, aatypeu=None, aatyp
            aarepair=aarepar, postaa=postaa, src=src, stabilize=stabilize, down8=down8, showmask=showmask, opencl=opencl, opencl_device=opencl_device))
 
 '''
-Tool to highlight black lines on borders by placing them in the middle. Such lines should then create a straight line in the middle, or even a cross if top and bottom lines line up accordingly.
+Tool to highlight black lines on borders and place them in the middle. Such lines should then create a straight line or even a cross if horizontal and vertical lines line up accordingly.
 The threshold is set for very dark black lines, so it has to be adjusted for detecting dirty lines.  There are also only default values for one or two byte depths.
 
-Optional "fix" included.  It's definitely recommended to do this shit manually, though.
+Optional "fix" included.  It's definitely recommended to do this manually, though.
+The fix depends on FillBorders and applies it to the highlighted areas on every clip.  This can potentially damage unaffected areas.
+I might write a protect for this eventually, but I only really care about the detection, not the fix.
 '''
 def black_detect(clip, thresh=None, fix=False, left=0, right=0, top=0, bottom=0):
 	if thresh == None:
@@ -78,7 +88,7 @@ def black_detect(clip, thresh=None, fix=False, left=0, right=0, top=0, bottom=0)
 		elif clip.format.bits_per_sample == 16:
 			thresh = 5000
 		else:
-			raise TypeError("dirt: thresh argument required for bit depths outside 8 and 16.")
+			raise TypeError("black_detect: thresh argument required for bit depths outside 8 and 16.")
 	if fix == False:
 		mask = core.std.ShufflePlanes(clip, 0, vs.GRAY).std.Binarize("{0}".format(thresh)).std.Invert().std.Maximum().std.Inflate().std.Maximum().std.Inflate()
 		l = core.std.Crop(mask, right=clip.width/2)
@@ -96,15 +106,15 @@ def black_detect(clip, thresh=None, fix=False, left=0, right=0, top=0, bottom=0)
 '''
 Stupid downscaling that should automatically calculate the correct width and height.
 '''
-def ds(clip, nocrop, size=720):
+def ds(clip, size=720, sar=16/9):
     ar = clip.width / clip.height
-    if ar > (16 / 9):
+    if ar > (sar):
     	width = size * ar
         height = size
     	w = round((width * ar) / 2) * 2
     	h = round((height * (1 / ar)) / 2) * 2
-    if ar < (16 / 9):
-    	width = size * 9 / 16
+    if ar < (sar):
+    	width = size * (sar**(-1))
         height = size * ar
     	w = round((width * ar) / 2) * 2
     	h = round((height * (1 / ar)) / 2) * 2
