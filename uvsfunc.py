fbef904c115d6f6d691ed81321eba979cf4ef078
Ututu <daoko@protonmail.com>
2019-01-12 12:51:01 +0000

    Update uvsfunc.py

--- uvsfunc.py
+++ uvsfunc.py
@@ -12,20 +12,19 @@ def rekt(src, fix, left=0, top=0, right=0, bottom=0):
 		# if get_subsampling(src) == '422':
 			# if src.width-left-right % 2 != 0:
 				# raise ValueError("rekt: fix width must be mod 2 if source clip's color space is YUV422.")
-	#else:
-		if left > 0 or right > 0:
-			m = core.std.Crop(fix, left=left, right=right)
-			l = core.std.Crop(src, right=src.width - left) if left > 0 else 0
-			r = core.std.Crop(src, left=src.width - right) if right > 0 else 0
-			params = [x for x in [l, m, r] if x != 0]
-			m = core.std.StackHorizontal(params)
-		if top > 0 or bottom > 0:
-			t = core.std.Crop(src, bottom=src.height - top) if top > 0 else 0
-			m = core.std.Crop(m, bottom=bottom, top=top)
-			b = core.std.Crop(src, top=src.height - bottom) if bottom > 0 else 0
-			params = [x for x in [t, m, b] if x != 0]
-			m = core.std.StackVertical(params)
-		return m
+	if left > 0 or right > 0:
+		m = core.std.Crop(fix, left=left, right=right)
+		l = core.std.Crop(src, right=src.width - left) if left > 0 else 0
+		r = core.std.Crop(src, left=src.width - right) if right > 0 else 0
+		params = [x for x in [l, m, r] if x != 0]
+		m = core.std.StackHorizontal(params)
+	if top > 0 or bottom > 0:
+		t = core.std.Crop(src, bottom=src.height - top) if top > 0 else 0
+		m = core.std.Crop(m, bottom=bottom, top=top)
+		b = core.std.Crop(src, top=src.height - bottom) if bottom > 0 else 0
+		params = [x for x in [t, m, b] if x != 0]
+		m = core.std.StackVertical(params)
+	return m
   
 '''
 Creates a rectangular "mask" in which a fix is applied to only the masked area.  This vastly speeds up filters like anti-aliasing and scaling.
@@ -46,23 +45,22 @@ def rekt_fast(src, fun=lambda x: x, left=0, top=0, right=0, bottom=0):
 		# if get_subsampling(src) == '422':
 			# if src.width-left-right % 2 != 0:
 				# raise ValueError("rekt_fast: fix width must be mod 2 if source clip's color space is YUV422.")
-	#else:
-		m = core.std.Crop(src, left=left, right=right, bottom=bottom, top=top)
-		if left > 0 or right > 0:
-			m = fun(m).std.AddBorders(top=top, bottom=bottom)
-			l = core.std.Crop(src, right=src.width - left) if left > 0 else 0
-			r = core.std.Crop(src, left=src.width - right) if right > 0 else 0
-			params = [x for x in [l, m, r] if x != 0]
-			m = core.std.StackHorizontal(params)
-		else:
-			m = fun(m).std.AddBorders(right=right, left=left)
-		if top > 0 or bottom > 0:
-			t = core.std.Crop(src, bottom=src.height - top) if top > 0 else 0
-			m = core.std.Crop(m, bottom=bottom, top=top)
-			b = core.std.Crop(src, top=src.height - bottom) if bottom > 0 else 0
-			params = [x for x in [t, m, b] if x != 0]
-			m = core.std.StackVertical(params)
-		return m
+	m = core.std.Crop(src, left=left, right=right, bottom=bottom, top=top)
+	if left > 0 or right > 0:
+		m = fun(m).std.AddBorders(top=top, bottom=bottom)
+		l = core.std.Crop(src, right=src.width - left) if left > 0 else 0
+		r = core.std.Crop(src, left=src.width - right) if right > 0 else 0
+		params = [x for x in [l, m, r] if x != 0]
+		m = core.std.StackHorizontal(params)
+	else:
+		m = fun(m).std.AddBorders(right=right, left=left)
+	if top > 0 or bottom > 0:
+		t = core.std.Crop(src, bottom=src.height - top) if top > 0 else 0
+		m = core.std.Crop(m, bottom=bottom, top=top)
+		b = core.std.Crop(src, top=src.height - bottom) if bottom > 0 else 0
+		params = [x for x in [t, m, b] if x != 0]
+		m = core.std.StackVertical(params)
+	return m
 
 '''
 Anti-aliasing alias.
