e818d14e485619b2f82e1f038fa65c807cd83eb4
Ututu <daoko@protonmail.com>
2020-04-28 17:28:19 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from setuptools import setup
 
-setup(name='rekt', version='r29', description='VapourSynth wrapper for creating rectangular masks with Crop and Stack.',
-      long_description='README.md', url='https://gitlab.com/Ututu/rekt', author='Ututu',
-      author_email='daoko@protonmail.com', install_requires=['vapoursynth'], packages=['rekt'], zip_safe=False)
+setup(name='rekt',
+      version='r30',
+      description='VapourSynth wrapper for Cropping and Stacking clips.',
+      long_description='README.md',
+      url='https://gitlab.com/Ututu/rekt',
+      author='Ututu',
+      author_email='daoko@protonmail.com',
+      install_requires=['vapoursynth'],
+      packages=['rekt'],
+      zip_safe=False)
