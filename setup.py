2fbd63bd59942a1a4f193e7a927d8088d1bdc15c
quietvoid <39477805+quietvoid@users.noreply.github.com>
2022-06-05 19:00:25 -0400

    Update setup

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
 
-setup(name='rekt',
-      version='1.0',
-      description='VapourSynth wrapper for Cropping and Stacking clips.',
-      long_description='README.md',
-      url='https://gitlab.com/Ututu/rekt',
-      author='Ututu',
-      author_email='daoko@protonmail.com',
-      install_requires=['vapoursynth', 'vsutil'],
-      packages=['rekt'],
-      zip_safe=False)
+setup(
+    name='rekt',
+    version='1.0.0',
+    description='VapourSynth wrapper for Cropping and Stacking clips.',
+    long_description='README.md',
+    url='https://github.com/OpusGang/rekt',
+    author='OpusGang',
+    packages=['rekt'],
+    install_requires=[
+        'VapourSynth>=57',
+        'vsutil @ git+https://github.com/Irrational-Encoding-Wizardry/vsutil.git@956fa579406ca9edf6e0b6a834defae28efb51ce'
+    ],
+    zip_safe=False
+)
