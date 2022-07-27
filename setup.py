db889a53ecbdf7d7c592d8738d95b4901a80f851
noizuy <85082087+noizuy@users.noreply.github.com>
2022-07-27 15:12:09 +0200

    rename for pypi

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
 setup(
-    name='rekt',
+    name='vs-rekt',
     version='1.0.0',
     description='VapourSynth wrapper for Cropping and Stacking clips.',
-    long_description='README.md',
     url='https://github.com/OpusGang/rekt',
     author='OpusGang',
     packages=['rekt'],
+    license="MIT",
     install_requires=[
         'VapourSynth>=57',
-        'vsutil==0.7.0',
+        'vsutil>=0.7.0',
     ],
     zip_safe=False
 )
